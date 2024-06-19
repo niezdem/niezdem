d6f1b107bf7a2f83c0d9cf8e69db4580b67a1fb2
Dmitriy Yakovlev <yakovlev.dragomir@gmail.com>
2024-06-19 22:24:49 +0200

    fix: text changes

--- components/AboutMeWidget.tsx
+++ components/AboutMeWidget.tsx
@@ -5,7 +5,7 @@ const AboutMeWidget = () => (
   <Card title="About me" className="sm:col-span-2">
     <Text>
       Hey there! I&apos;m <Text backlight>Dima</Text> - your go-to guy for all
-      things web design and frontend development. With a solid 6+ years under my
+      things web design and frontend development. With a solid 5+ years under my
       belt, I&apos;m all about jazzing up teamwork and smoothing out the convo
       between designers and developers. Why? Because who likes waiting ages for
       projects to finish? Not me, and I&apos;m guessing not you either. My
