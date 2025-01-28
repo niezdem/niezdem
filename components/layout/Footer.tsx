3779ff563b11d14efa83481f8a31d063cf0be54f
Dmitr Niezdemkowski <git@niezdem.com>
2025-01-28 01:06:56 +0100

    chore: update CV and footer text

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,8 +4,11 @@ import Text from '@/components/ui/Text';
 const Footer = () => {
   return (
     <footer className="n-container mt-10 flex flex-shrink-0 items-center justify-center gap-2 border-t border-zinc-100/5 py-4 md:mt-20">
-      <Logo width={30} height={30} />
-      <Text size="sm">2025 nezdem.com ©</Text>
+      <Logo width={30} height={30} className="shrink-0" />
+      <Text size="sm" align="center">
+        2025. Stealing ideas isn&apos;t ©ool. Reach out if you want to get
+        inspired the right way.
+      </Text>
     </footer>
   );
 };
