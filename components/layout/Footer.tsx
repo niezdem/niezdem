7d7eb22bedc1d95d5616d7323fab51c6768e3554
Dmitr Niezdemkowski <git@niezdem.com>
2026-01-06 00:47:01 +0100

    chore: update CV

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="n-container mt-10 flex flex-shrink-0 items-center justify-center gap-2 border-t border-zinc-100/5 py-4 md:mt-20">
       <Logo width={30} height={30} className="shrink-0" />
       <Text size="sm" align="center">
-        2025. Stealing ideas isn&apos;t ©ool. Reach out if you want to get
+        2026. Stealing ideas isn&apos;t ©ool. Reach out if you want to get
         inspired the right way.
       </Text>
     </footer>
